3e8119d771af679b701b9a111192bf71548978a7
Piotr Wolinski <piotr.wolinski.2017@outlook.com>
2025-05-05 11:52:25 +0200

    Make colmap point cloud init assume multiple camera instrinsics

--- multipleviewprogress.sh
+++ multipleviewprogress.sh
@@ -1,7 +1,9 @@
 workdir=$1
+
 python scripts/extractimages.py multipleview/$workdir
-colmap feature_extractor --database_path ./colmap_tmp/database.db --image_path ./colmap_tmp/images  --SiftExtraction.max_image_size 4096 --SiftExtraction.max_num_features 16384 --SiftExtraction.estimate_affine_shape 1 --SiftExtraction.domain_size_pooling 1
+colmap feature_extractor --database_path ./colmap_tmp/database.db --image_path ./colmap_tmp/images --ImageReader.single_camera_per_folder 1 --SiftExtraction.max_image_size 4096 --SiftExtraction.max_num_features 16384 --SiftExtraction.estimate_affine_shape 1 --SiftExtraction.domain_size_pooling 1
 colmap exhaustive_matcher --database_path ./colmap_tmp/database.db
+
 mkdir ./colmap_tmp/sparse
 colmap mapper --database_path ./colmap_tmp/database.db --image_path ./colmap_tmp/images --output_path ./colmap_tmp/sparse
 mkdir ./data/multipleview/$workdir/sparse_
--- scripts/extractimages.py
+++ scripts/extractimages.py
@@ -2,21 +2,24 @@ import os
 import sys
 import shutil
 
+from pathlib import Path
+
 folder_path = sys.argv[1]
 
 colmap_path = "./colmap_tmp"
 images_path = os.path.join(colmap_path, "images")
 os.makedirs(images_path, exist_ok=True)
-i=0
 
 dir1=os.path.join("data",folder_path)
-for folder_name in sorted(os.listdir(dir1)):
-    dir2=os.path.join(dir1,folder_name)
-    for file_name in os.listdir(dir2):
-        if file_name.startswith("frame_00001"):
-            i=i+1
-            src_path = os.path.join(dir2, file_name)
-            dst_path = os.path.join(images_path, f"image{i}.jpg")
-            shutil.copyfile(src_path, dst_path) 
+for camera_id in sorted(os.listdir(dir1)):
+    camera_dir = os.path.join(dir1, camera_id)
+    if Path(camera_dir).is_dir():
+        for file_name in os.listdir(camera_dir):
+            if file_name.startswith("frame_00001"):
+                src_path = os.path.join(camera_dir, file_name)
+                dst_dir = os.path.join(images_path, camera_id)
+                os.mkdir(dst_dir)
+                dst_path = os.path.join(dst_dir, f"frame_00001.jpg")
+                shutil.copyfile(src_path, dst_path) 
 
-print("End！")
+print("Done copying inital frames for 3D structure initialization.")
