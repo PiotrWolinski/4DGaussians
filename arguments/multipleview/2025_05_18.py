8fd69d11df334035d68304dc03437ada569e60bb
Piotr Wolinski <piotr.wolinski.2017@outlook.com>
2025-05-29 11:17:06 +0200

    Add missing iterations on arguments

---
+++ arguments/multipleview/2025_05_18.py
@@ -0,0 +1,34 @@
+ModelHiddenParams = dict(
+    kplanes_config = {
+     'grid_dimensions': 2,
+     'input_coordinate_dim': 4,
+     'output_coordinate_dim': 16,
+     'resolution': [64, 64, 64, 10]
+    },
+    multires = [1],
+    defor_depth = 0,
+    net_width = 128,
+    plane_tv_weight = 0.0002,
+    time_smoothness_weight = 0.001,
+    l1_time_planes =  0.0001,
+    bounds=1.0,
+    no_do=False,
+    no_dshs=False,
+    no_ds=False,
+    render_process=True,
+    static_mlp=False
+)
+
+OptimizationParams = dict(
+    dataloader=False,
+    iterations = 40_000,
+    batch_size=2,
+    coarse_iterations = 15_000,
+    densify_until_iter = 15_000,
+    opacity_reset_interval = 3000,
+    opacity_threshold_coarse = 0.005,
+    opacity_threshold_fine_init = 0.005,
+    opacity_threshold_fine_after = 0.005,
+    position_lr_init=0.001,
+    # pruning_interval = 2000
+)
---
+++ arguments/multipleview/2025_05_26_undist.py
@@ -0,0 +1,34 @@
+ModelHiddenParams = dict(
+    kplanes_config = {
+     'grid_dimensions': 2,
+     'input_coordinate_dim': 4,
+     'output_coordinate_dim': 16,
+     'resolution': [64, 64, 64, 50]
+    },
+    multires = [1],
+    defor_depth = 1,
+    net_width = 128,
+    plane_tv_weight = 0.0002,
+    time_smoothness_weight = 0.01,
+    l1_time_planes =  0.0005,
+    # bounds=1.0,
+    no_do=False,
+    no_dshs=False,
+    no_ds=False,
+    render_process=True,
+    static_mlp=False
+)
+
+OptimizationParams = dict(
+    dataloader=True,
+    iterations = 40_000,
+    batch_size=2,
+    coarse_iterations = 30_000,
+    densify_until_iter = 14_000,
+    opacity_reset_interval = 100_000,
+    opacity_threshold_coarse = 0.005,
+    opacity_threshold_fine_init = 0.005,
+    opacity_threshold_fine_after = 0.005,
+    position_lr_init=0.001,
+    # pruning_interval = 2000
+)
